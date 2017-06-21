7cf9c4be63b0383f1d01cf28317b67d648139935
quytm <minhquylt95@gmail.com>
2017-06-21 16:00:25 +0700

    builder design pattern

---
+++ src/com/tmq/designpattern/creational/builder/MainBuilder.java
@@ -0,0 +1,15 @@
+package com.tmq.designpattern.creational.builder;
+
+/**
+ * Created by quytm on 6/21/2017.
+ */
+public class MainBuilder {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer.ComputerBuilder("500 GB", "2 GB")
+                .setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true).build();
+
+    }
+
+}
