723e6b41c9e222536c40214c5a19be9cf8988bfb
quytm <minhquylt95@gmail.com>
2017-06-21 17:13:38 +0700

    test clone and copy data in Factory pattern

--- src/com/tmq/designpattern/creational/prototype/MainPrototypePattern.java
+++ src/com/tmq/designpattern/creational/prototype/MainPrototypePattern.java
@@ -24,6 +24,25 @@ public class MainPrototypePattern {
         System.out.println("emps List: "+emps.getEmpList());
         System.out.println("empsNew List: "+list);
         System.out.println("empsNew1 List: "+list1);
+
+        testCloneAndCopy();
+    }
+
+    private static void testCloneAndCopy() {
+        Employees employees = new Employees();
+        employees.loadData();
+
+        Employees copyEmployees = new Employees(employees.getEmpList());
+
+        System.out.println("--- Before ---");
+        System.out.println("employees: \t\tlist = " + employees.getEmpList());
+        System.out.println("copyEmployees: \tlist = " + copyEmployees.getEmpList());
+
+        System.out.println("--- After ---");
+        employees.getEmpList().add("new_object_in_employees");
+        copyEmployees.getEmpList().add("new_object_in_copyEmployees");
+        System.out.println("employees: \t\tlist = " + employees.getEmpList());
+        System.out.println("copyEmployees: \tlist = " + copyEmployees.getEmpList());
     }
 
 }
